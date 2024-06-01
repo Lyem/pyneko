a7d56c2920ad6dcfe2a42113d576ed097a483d08
lyem <rfael403@gmail.com>
2024-05-31 23:55:01 -0300

    add luratoons

---
+++ src/core/providers/infra/pt_Br/luratoons/__init__.py
@@ -0,0 +1,11 @@
+from core.providers.infra.template.scan_madara_clone import ScanMadaraClone
+
+class LuratoonsProvider(ScanMadaraClone):
+    name = 'luratoons'
+    icon = 'https://i.imgur.com/ycuyRsy.png'
+    icon_hash = 'T3mBA4AkUz9sptRplgCb9VU7iHiQiYc'
+    lang = 'pt-Br'
+    domain = 'luratoons.com'
+
+    def __init__(self):
+        self.url = 'https://luratoons.com/'   

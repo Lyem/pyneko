e1e8ca41b44deb13e6328fc3c82018764bd84bf4
lyem <rfael403@gmail.com>
2024-06-03 21:36:36 -0300

    add build folders and file to clean remove

--- scripts/remove_cache.py
+++ scripts/remove_cache.py
@@ -5,8 +5,11 @@ root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 def remove_cache_folders():
     for root, dirs, files in os.walk(root_path):
+        for file in files:
+            if file == '__init__.spec':
+                os.remove(file)
         for folder in dirs:
-            if folder == "__pycache__" or folder == ".pytest_cache":
+            if folder == "__pycache__" or folder == ".pytest_cache" or folder == "build" or folder == "dist":
                 folder_path = os.path.join(root, folder)
                 shutil.rmtree(folder_path)
 

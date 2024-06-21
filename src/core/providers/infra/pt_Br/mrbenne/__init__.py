21244eec3657a53329cf6a9b1b84c834a5509d4d
lyem <rfael403@gmail.com>
2024-06-21 19:26:58 -0300

    add MrYaoiFansubProvider

---
+++ src/core/providers/infra/pt_Br/mrbenne/__init__.py
@@ -0,0 +1,21 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class MrYaoiFansubProvider(WordPressMadara):
+    name = 'mrbenne'
+    icon = 'https://i.imgur.com/ycuyRsy.png'
+    icon_hash = 'T3mBA4AkUz9sptRplgCb9VU7iHiQiYc'
+    lang = 'pt-Br'
+    domain = 'mrbenne.com'
+
+    def __init__(self):
+        self.url = 'https://mrbenne.com'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'
+    

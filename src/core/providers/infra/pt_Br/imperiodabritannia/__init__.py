1af6bb95f428a5b6042084dd0aa1596943e22ac4
lyem <rfael403@gmail.com>
2024-07-02 23:24:14 -0300

    fix imperio britania

--- src/core/providers/infra/pt_Br/imperiodabritannia/__init__.py
+++ src/core/providers/infra/pt_Br/imperiodabritannia/__init__.py
@@ -2,6 +2,14 @@ from fake_useragent import UserAgent
 from core.providers.domain.entities import Pages
 from core.download.application.use_cases import DownloadUseCase
 from core.providers.infra.template.wordpress_madara import WordPressMadara
+import re
+from typing import List
+from bs4 import BeautifulSoup
+from core.__seedwork.infra.http import Http
+from core.providers.infra.template.base import Base
+from core.__seedwork.infra.http.contract.http import Response
+from core.providers.domain.entities import Chapter, Pages, Manga
+from urllib.parse import urljoin, urlencode, urlparse, urlunparse, parse_qs
 
 class ImperiodabritanniaProvider(WordPressMadara):
     name = 'imperiodabritannia'
@@ -23,6 +31,7 @@ class ImperiodabritanniaProvider(WordPressMadara):
         self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'
         ua = UserAgent()
         user = ua.chrome
+        self.user = ua.chrome
         self.headers = {'host': 'imperiodabritannia.com', 'user_agent': user, 'referer': f'{self.url}'}
     
     def download(self, pages: Pages, fn: any, headers=None, cookies=None):
@@ -31,3 +40,12 @@ class ImperiodabritanniaProvider(WordPressMadara):
         else:
             headers = self.headers
         DownloadUseCase().execute(pages=pages, fn=fn, headers=headers, cookies=cookies)
+    
+    def _get_chapters_ajax(self, manga_id):
+        uri = urljoin(self.url, f'{manga_id}ajax/chapters/')
+        response = Http.post(uri, headers={'Cookie': 'visited=true; wpmanga-reading-history=W3siaWQiOjg4MiwiYyI6IjIzMzU5IiwicCI6MSwiaSI6IiIsInQiOjE3MTk5NjEwODN9XQ%3D%3D'})
+        data = self._fetch_dom(response, self.query_chapters)
+        if data:
+            return data
+        else:
+            raise Exception('No chapters found (new ajax endpoint)!')

70cabb2dc9bebec557f50009380bf46febe050f2
lyem <rfael403@gmail.com>
2024-07-11 16:49:56 -0300

    add winterscan

---
+++ src/core/providers/infra/pt_Br/winterscan/__init__.py
@@ -0,0 +1,20 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class WinterScanProvider(WordPressMadara):
+    name = 'winterscan'
+    icon = 'https://i.imgur.com/ycuyRsy.png'
+    icon_hash = 'T3mBA4AkUz9sptRplgCb9VU7iHiQiYc'
+    lang = 'pt-Br'
+    domain = 'winterscan.com'
+
+    def __init__(self):
+        self.url = 'https://winterscan.com'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break.no-gaps'
+        self.query_title_for_uri = 'div.post-title > h1'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

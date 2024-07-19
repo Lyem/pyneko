22ade43eac36aa90597ac483e28ce384ffa42310
lyem <rfael403@gmail.com>
2024-07-19 12:50:42 -0300

    Update start.py
    fix win start script

--- scripts/start.py
+++ scripts/start.py
@@ -1,16 +1,22 @@
+import os
 import subprocess
 from pathlib import Path
-import os
+from sys import platform
 
+is_posix = platform.startswith(("darwin", "cygwin", "linux", "linux2"))
 
 def run():
     src_path = Path(__file__).resolve().parent.parent / 'src'
 
-    os.environ['PYTHONPATH'] = str(src_path)
-
     app = Path(__file__).resolve().parent.parent / 'src' / 'GUI_qt' / '__init__.py'
 
-    result = subprocess.run(["python", str(app)], capture_output=True, text=True, env=os.environ)
+    if(is_posix):
+        os.environ['PYTHONPATH'] = str(src_path)
+
+        result = subprocess.run(["python", str(app)], capture_output=True, text=True, env=os.environ)
+    else:
+        script = f'set PYTHONPATH={str(src_path)} && python {str(app)}'
+        result = subprocess.run(script, capture_output=True, text=True, shell=True)
 
     output = result.stdout
     error = result.stderr
@@ -21,3 +27,6 @@ def run():
     if error:
         print("Errors app:")
         print(error)
+
+if __name__ == "__main__":
+    run()

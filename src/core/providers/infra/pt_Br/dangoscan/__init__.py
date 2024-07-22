289ce359936a535fbb1de2e7a3ef34b1361ecfd3
lyem <rfael403@gmail.com>
2024-07-22 10:50:44 -0300

    dango scan

---
+++ src/core/providers/infra/pt_Br/dangoscan/__init__.py
@@ -0,0 +1,11 @@
+from core.providers.infra.template.scan_madara_clone import ScanMadaraClone
+
+class DangoScanProvider(ScanMadaraClone):
+    name = 'dangoscan'
+    icon = 'https://i.imgur.com/ycuyRsy.png'
+    icon_hash = 'T3mBA4AkUz9sptRplgCb9VU7iHiQiYc'
+    lang = 'pt-Br'
+    domain = 'dangoscan.com.br'
+
+    def __init__(self):
+        self.url = 'https://dangoscan.com.br'   

63e2d445880295d4067663fc818ba2cf48f74c6c
lyem <rfael403@gmail.com>
2024-09-12 20:28:30 -0300

    feat: add mode scanlator

---
+++ src/core/providers/infra/pt_Br/modescanlator/__init__.py
@@ -0,0 +1,68 @@
+from typing import List
+from bs4 import BeautifulSoup
+from core.__seedwork.infra.http import Http
+from core.providers.infra.template.base import Base
+from core.providers.domain.entities import Chapter, Pages, Manga
+
+class ModeScanlatorProvider(Base):
+    name = 'Mode Scanlator'
+    icon = 'https://i.imgur.com/EvOBEp6.png'
+    icon_hash = 'AAiCBQAjpQ1HiXl0aQt+p9kgJ0iFeYDKVw'
+    lang = 'pt-Br'
+    domain = 'modescanlator.net'
+
+    def __init__(self) -> None:
+        self.api = 'https://api.modescanlator.net'
+        self.reader = 'https://site.modescanlator.net/series/'
+    
+    def getMangas(self) -> List[Manga]:
+        response = Http.get(f'{self.api}/query?adult=true&page=1')
+        data = response.json()
+        page = 1
+        mangas = []
+        while data['meta']['next_page_url'] != None:
+            response = Http.get(f'{self.api}/query?adult=true&page={page}')
+            data = response.json()
+            for manga in data['data']:
+                mangas.append(Manga(manga['id'], manga['title']))
+            page += 1
+        return mangas
+    
+    def getManga(self, link: str) -> Manga:
+        if link.endswith('/'):
+            link = link[:-1]
+        id = link.split("/").pop();
+        response = Http.get(f'{self.api}/query?adult=true&page=1&perPage=999999999999999999')
+        data = response.json()
+        for manga in data['data']:
+            if manga['series_slug'] == id:
+                return Manga(manga['id'], manga['title'])
+        
+
+    def getChapters(self, id: str) -> List[Chapter]:
+        manga_response = Http.get(f'{self.api}/query?adult=true&page=1&perPage=999999999999999999')
+        manga_data = manga_response.json()
+        response = Http.get(f'{self.api}/chapter/query?page=1&perPage=999999999999999999&series_id={id}')
+        data = response.json()
+        list = []
+        name = ''
+        for manga in manga_data['data']:
+            if manga['id'] == id:
+                name = manga['title']
+        for chapter in data['data']:
+            list.append(Chapter(f'{chapter['series']['series_slug']}/{chapter['chapter_slug']}',chapter['chapter_name'], name))
+        list.reverse()
+        return list
+    
+    def getPages(self, ch: Chapter) -> Pages:
+        response = Http.get(f'{self.reader}{ch.id}')
+        soup = BeautifulSoup(str(response.content), 'html.parser')
+        imgs = soup.select('div.flex.flex-col.justify-center.items-center > img')
+        list = []
+        for img in imgs:
+            if img.get('src'):
+                if img.get('src') != '/icon.png':
+                    list.append(img.get('src'))
+            else: 
+                list.append(img.get('data-src'))
+        return Pages(ch.id, ch.number, ch.name, list)

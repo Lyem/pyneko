9b339e63f140a4f1c71cb34d687ad22c51b95c32
lyem <rfael403@gmail.com>
2024-09-15 19:32:21 -0300

    feat: add sss scanlator

---
+++ src/core/providers/infra/pt_Br/sssscanlator/__init__.py
@@ -0,0 +1,18 @@
+from core.providers.infra.template.manga_reader_cms import MangaReaderCms
+
+class SssScanlatorProvider(MangaReaderCms):
+    name = 'Sss Scanlator'
+    icon = 'https://i.imgur.com/QRjE79s.png'
+    icon_hash = 'd/iFDQIoqraAa360R1NPCZWlHiugekWiJw'
+    lang = 'pt-Br'
+    domain = 'sssscanlator.com.br'
+
+    def __init__(self):
+        super().__init__()
+        self.url = 'https://sssscanlator.com.br'
+        self.path = '/'
+
+        self.query_mangas = 'ul.manga-list li a'
+        self.query_chapters = 'div#chapterlist ul li'
+        self.query_pages = 'div#readerarea img'
+        self.query_title_for_uri = 'h1.entry-title'

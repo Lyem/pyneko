168c426dc37288bace74e994ca87126c2012bca3
lyem <rfael403@gmail.com>
2024-09-16 15:22:33 -0300

    feat: add neroxus provider

---
+++ src/core/providers/infra/pt_Br/neroxus/__init__.py
@@ -0,0 +1,20 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class NeroxusProvider(WordPressMadara):
+    name = 'Neroxus'
+    icon = 'https://i.imgur.com/ycuyRsy.png'
+    icon_hash = 'T3mBA4AkUz9sptRplgCb9VU7iHiQiYc'
+    lang = 'pt-Br'
+    domain = 'neroxus.com.br'
+
+    def __init__(self):
+        self.url = 'https://neroxus.com.br'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break.no-gaps'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

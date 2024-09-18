68ca8d054789b1f54ffc1fc1fa11557f86ac0563
lyem <rfael403@gmail.com>
2024-09-17 21:57:37 -0300

    feat: add ninja Comics

---
+++ src/core/providers/infra/pt_Br/ninjacomics/__init__.py
@@ -0,0 +1,20 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class NinjaComicsProvider(WordPressMadara):
+    name = 'Ninja Comics'
+    icon = 'https://i.imgur.com/ycuyRsy.png'
+    icon_hash = 'T3mBA4AkUz9sptRplgCb9VU7iHiQiYc'
+    lang = 'pt_Br'
+    domain = 'ninjacomics.xyz'
+
+    def __init__(self):
+        self.url = 'https://ninjacomics.xyz'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break.no-gaps'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

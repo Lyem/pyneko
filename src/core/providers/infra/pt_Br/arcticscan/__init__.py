9e01bcbf6925b3d0569e518afc03eb31db627b02
YanEmmanuel <yanemmanuel00@gmail.com>
2024-10-16 18:45:46 -0300

    feat: vortecescans && outra ai

---
+++ src/core/providers/infra/pt_Br/arcticscan/__init__.py
@@ -0,0 +1,20 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class ArcticScanProvider(WordPressMadara):
+    name = 'Arctic Scan'
+    icon = 'https://i.imgur.com/ycuyRsy.png'
+    icon_hash = 'T3mBA4AkUz9sptRplgCb9VU7iHiQiYc'
+    lang = 'pt_Br'
+    domain = 'arcticscan.top'
+
+    def __init__(self):
+        self.url = 'https://arcticscan.top/'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

8e8eea044dab07363730c53ab003cc99a05bb42c
lyem <rfael403@gmail.com>
2024-10-20 14:47:37 -0300

    fix: macos build

--- scripts/build.py
+++ scripts/build.py
@@ -4,6 +4,7 @@ from sys import platform
 import os
 
 is_posix = platform.startswith(("darwin", "cygwin", "linux", "linux2"))
+is_mac = platform.system().startswith('Darwin')
 
 src_path = Path(__file__).resolve().parent.parent / 'src'
 path_to_main = str(src_path / 'GUI_qt' / '__init__.py')
@@ -14,28 +15,32 @@ splash = 'splash.jpg' if is_posix else 'splash.png'
 
 os.environ['PYTHONPATH'] = str(src_path)
 
+args = [
+    path_to_main,
+    '--onefile',
+    '--noconfirm',
+    '--noconsole',
+    '--collect-all=PyQt6',
+    '--collect-all=nodriver',
+    '--collect-all=bs4',
+    '--collect-all=httpx',
+    '--collect-all=cv2',
+    '--collect-all=cloudscraper',
+    '--collect-all=waifu2x-ncnn-py',
+    '--collect-all=fake_useragent',
+    '--collect-all=pillow_avif',
+    f'--icon=assets/icon.ico',
+    '--exclude-module=tests',
+    f'--add-data=src/core/providers/infra{separator}core/providers/infra',
+    f'--add-data=src/core/__seedwork/infra{separator}core/__seedwork/infra',
+    f'--add-data=src/core/cloudflare{separator}core/cloudflare',
+    f'--add-data=src/core/config{separator}core/config',
+    f'--add-data=src/core/download{separator}core/download',
+    f'--add-data=src/GUI_qt/assets{separator}GUI_qt/assets'
+]
+
+if not is_mac:
+    args.append(f'--splash=assets/{splash}')
+
 def install():
-    PyInstaller.__main__.run([
-        path_to_main,
-        '--onefile',
-        '--noconfirm',
-        '--noconsole',
-        '--collect-all=PyQt6',
-        '--collect-all=nodriver',
-        '--collect-all=bs4',
-        '--collect-all=httpx',
-        '--collect-all=cv2',
-        '--collect-all=cloudscraper',
-        '--collect-all=waifu2x-ncnn-py',
-        '--collect-all=fake_useragent',
-        '--collect-all=pillow_avif',
-        f'--icon=assets/icon.ico',
-        f'--splash=assets/{splash}',
-        '--exclude-module=tests',
-        f'--add-data=src/core/providers/infra{separator}core/providers/infra',
-        f'--add-data=src/core/__seedwork/infra{separator}core/__seedwork/infra',
-        f'--add-data=src/core/cloudflare{separator}core/cloudflare',
-        f'--add-data=src/core/config{separator}core/config',
-        f'--add-data=src/core/download{separator}core/download',
-        f'--add-data=src/GUI_qt/assets{separator}GUI_qt/assets'
-    ])
+    PyInstaller.__main__.run(args)

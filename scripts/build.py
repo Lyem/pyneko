e997d5abf8411a8f4e9747e9c07b7f9aa6886873
lyem <rfael403@gmail.com>
2024-10-20 14:49:19 -0300

    fix: build

--- scripts/build.py
+++ scripts/build.py
@@ -4,7 +4,7 @@ from sys import platform
 import os
 
 is_posix = platform.startswith(("darwin", "cygwin", "linux", "linux2"))
-is_mac = platform.system().startswith('Darwin')
+is_mac = platform.startswith('Darwin')
 
 src_path = Path(__file__).resolve().parent.parent / 'src'
 path_to_main = str(src_path / 'GUI_qt' / '__init__.py')

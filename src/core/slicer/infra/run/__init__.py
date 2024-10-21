887d7a59cc7c65d0141ea141b4585e3a6340fb95
lyem <rfael403@gmail.com>
2024-10-20 21:34:26 -0300

    optimize: slicer

--- src/core/slicer/infra/run/__init__.py
+++ src/core/slicer/infra/run/__init__.py
@@ -1,11 +1,17 @@
 import gc
 import math
+import shutil
+import tempfile
+import pillow_avif
 from pathlib import Path
+from PIL import Image as pil
+pil.Image.MAX_IMAGE_PIXELS = 933120000
 from core.config.img_conf import get_config
 from core.slicer.infra.detectors import select_detector
 from core.download.domain.dowload_entity import Chapter
 from core.slicer.infra.utils.constants import WIDTH_ENFORCEMENT
 from core.slicer.infra.services import ImageHandler, ImageManipulator
+import tempfile
 
 class SmartStitch():
     def run(self, ch: Chapter, fn = None) -> Chapter:
@@ -19,10 +25,51 @@ class SmartStitch():
             width_enforce_mode = WIDTH_ENFORCEMENT.AUTOMATIC
         else:
             width_enforce_mode = WIDTH_ENFORCEMENT.NONE
-        imgs = img_handler.load(ch.files)
-        imgs = img_manipulator.resize(
-            imgs, width_enforce_mode, conf.custom_width
-        )
+        files = []
+        continue_index = 0
+        img_iteration = 1
+        while continue_index < len(ch.files) - 1:
+            group_height = 0
+            img_objs = []
+            for i in range(continue_index, len(ch.files)):
+                img = pil.open(ch.files[i])
+                img_objs.append(img)
+                width, height = img.size
+                group_height += height
+                if group_height >= 5 * conf.split_height or i == len(ch.files) - 1:
+                    if continue_index < len(ch.files) - 1:
+                        continue_index = i + 1
+                    if i == len(ch.files):
+                        continue_index = len(ch.files) - 1
+                    break
+
+            imgs = img_manipulator.resize(
+                img_objs, width_enforce_mode, conf.custom_width
+            )
+            combined_img = img_manipulator.combine(imgs)
+            slice_points = detector.run(
+                combined_img,
+                conf.split_height,
+                sensitivity=conf.detection_senstivity,
+                ignorable_pixels=conf.ignorable_pixels,
+                scan_step=conf.scan_line_step,
+            )
+            imgs = img_manipulator.slice(combined_img, slice_points)
+            for img in imgs:
+                filename=img_handler.save(
+                    str(Path.joinpath(Path(tempfile.gettempdir()), 'pyneko', Path(ch.files[0]).parent.parent.name, f'{ch.number} [stitched]')),
+                    img,
+                    img_iteration,
+                    img_format=conf.img,
+                    quality=100,
+                )
+                files.append(str(Path.joinpath(Path(tempfile.gettempdir()), 'pyneko', Path(ch.files[0]).parent.parent.name, f'{ch.number} [stitched]', filename)))
+                img_iteration += 1
+
+            if fn != None:
+                fn(math.ceil(continue_index * 80)/len(ch.files))
+
+        imgs = img_handler.load(files)
         combined_img = img_manipulator.combine(imgs)
         slice_points = detector.run(
             combined_img,
@@ -31,9 +78,9 @@ class SmartStitch():
             ignorable_pixels=conf.ignorable_pixels,
             scan_step=conf.scan_line_step,
         )
-        imgs = img_manipulator.slice(combined_img, slice_points)
         files = []
         img_iteration = 1
+        imgs = img_manipulator.slice(combined_img, slice_points)
         for i, img in enumerate(imgs):
             filename=img_handler.save(
                 str(Path.joinpath(Path(ch.files[0]).parent.parent, f'{ch.number} [stitched]')),
@@ -42,14 +89,16 @@ class SmartStitch():
                 img_format=conf.img,
                 quality=100,
             )
+            if fn != None:
+                fn(80 + (math.ceil(i * 50)/len(ch.files)))
             files.append(str(Path.joinpath(Path(ch.files[0]).parent.parent, f'{ch.number} [stitched]', filename)))
             img_iteration += 1
-
-            if fn != None:
-                fn(math.ceil(i * 100)/len(imgs))
+        
+        shutil.rmtree(Path.joinpath(Path(tempfile.gettempdir()), 'pyneko', Path(ch.files[0]).parent.parent.name, f'{ch.number} [stitched]'))
 
         gc.collect()
+
         if fn != None:
-            fn(math.ceil(len(imgs) * 100)/len(imgs))
+            fn(math.ceil(len(ch.files) * 100)/len(ch.files))
         
         return Chapter(ch.number, files)

01686d5d3e143d9a3fb742123c1a81e641f902f7
lyem <rfael403@gmail.com>
2024-10-27 23:55:41 -0300

    feat: add hot reload

--- scripts/start.py
+++ scripts/start.py
@@ -14,9 +14,9 @@ def run():
         os.environ['PYTHONPATH'] = str(src_path)
         os.environ['PYNEKOENV'] = 'dev'
 
-        result = subprocess.run(["python", str(app)], capture_output=True, text=True, env=os.environ)
+        result = subprocess.run(["jurigged", str(app)], capture_output=True, text=True, env=os.environ)
     else:
-        script = f'set PYTHONPATH={str(src_path)} && set PYNEKOENV=dev && python {str(app)}'
+        script = f'set PYTHONPATH={str(src_path)} && set PYNEKOENV=dev && jurigged {str(app)}'
         result = subprocess.run(script, capture_output=True, text=True, shell=True)
 
     output = result.stdout

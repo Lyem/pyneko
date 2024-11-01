345024faac1ecac1c362f77cb61702af0c35f53f
lyem <rfael403@gmail.com>
2024-11-01 16:12:28 -0300

    fix: pynekoenv on windows

--- scripts/start.py
+++ scripts/start.py
@@ -10,13 +10,14 @@ def run():
 
     app = Path(__file__).resolve().parent.parent / 'src' / 'GUI_qt' / '__init__.py'
 
+    os.environ['PYNEKOENV'] = 'dev'
+
     if(is_posix):
         os.environ['PYTHONPATH'] = str(src_path)
-        os.environ['PYNEKOENV'] = 'dev'
 
         result = subprocess.run(["jurigged", str(app)], capture_output=True, text=True, env=os.environ)
     else:
-        script = f'set PYTHONPATH={str(src_path)} && set PYNEKOENV=dev && jurigged {str(app)}'
+        script = f"set PYTHONPATH={str(src_path)} && jurigged {str(app)}"
         result = subprocess.run(script, capture_output=True, text=True, shell=True)
 
     output = result.stdout

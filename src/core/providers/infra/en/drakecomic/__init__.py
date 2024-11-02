5ba665ddddf9862b695adbfea6fa5f20b880f365
lyem <rfael403@gmail.com>
2024-11-02 14:46:54 -0300

    feat: add drake comic

---
+++ src/core/providers/infra/en/drakecomic/__init__.py
@@ -0,0 +1,16 @@
+from core.providers.infra.template.manga_reader_cms import MangaReaderCms
+
+class DrakeComicProvider(MangaReaderCms):
+    name = 'Drake Comic'
+    lang = 'en'
+    domain = ['drakecomic.org']
+
+    def __init__(self):
+        super().__init__()
+        self.url = 'https://drakecomic.org/'
+        self.path = '/'
+
+        self.query_mangas = 'ul.manga-list li a'
+        self.query_chapters = 'div#chapterlist ul li'
+        self.query_pages = 'div#readerarea img'
+        self.query_title_for_uri = 'h1.entry-title'

276c2f7e2347fab3f23a401a2f9e5ae6446bf84a
lyem <rfael403@gmail.com>
2024-11-10 12:39:56 -0300

    feat: add ycscan

---
+++ src/core/providers/infra/pt_Br/ycscan/__init__.py
@@ -0,0 +1,18 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class YaoiComicsProvider(WordPressMadara):
+    name = 'Yaoi Comics (ycscan)'
+    lang = 'pt_Br'
+    domain = ['ycscan.com']
+
+    def __init__(self):
+        self.url = 'https://ycscan.com'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break.no-gaps'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

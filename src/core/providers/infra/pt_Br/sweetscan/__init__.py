a09aaafbc8ad277c4373d19118bba6dfefe65109
lyem <rfael403@gmail.com>
2024-11-13 12:43:00 -0300

    feat: sweet time scan

---
+++ src/core/providers/infra/pt_Br/sweetscan/__init__.py
@@ -0,0 +1,18 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class SweetScanProvider(WordPressMadara):
+    name = 'Sweet Scan'
+    lang = 'pt_Br'
+    domain = ['sweetscan.net']
+
+    def __init__(self):
+        self.url = 'https://sweetscan.net/'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break.no-gaps'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

2cfe683ff5759bf59c8e698e90b7f428ae6ab8db
MrZero <yanemmanuel00@gmail.com>
2024-11-24 21:59:29 -0300

    feat: AquaReader

---
+++ src/core/providers/infra/en/aquareader/__init__.py
@@ -0,0 +1,18 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class AquaReaderProvider(WordPressMadara):
+    name = 'AquaReader'
+    lang = 'en'
+    domain = ['aquareader.net']
+
+    def __init__(self):
+        self.url = 'https://aquareader.net/'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break.no-gaps'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

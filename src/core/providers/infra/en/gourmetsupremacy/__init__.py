24c03093821c0f58dc94a90d177cee640b98db4a
MrZero <yanemmanuel00@gmail.com>
2024-11-30 02:31:34 -0300

    feat: supremacy

---
+++ src/core/providers/infra/en/gourmetsupremacy/__init__.py
@@ -0,0 +1,18 @@
+from core.providers.infra.template.wordpress_madara import WordPressMadara
+
+class GourmetSupremacyProvider(WordPressMadara):
+    name = 'Gourmet Supremacy'
+    lang = 'en'
+    domain = ['gourmetsupremacy.com']
+
+    def __init__(self):
+        self.url = 'https://gourmetsupremacy.com/'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

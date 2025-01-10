da9870d314df3c57a847f7e437dc86e460555adc
lyem <rfael403@gmail.com>
2025-01-09 21:53:22 -0300

    fix: sussy

--- src/core/providers/infra/pt_Br/sussytoons/__init__.py
+++ src/core/providers/infra/pt_Br/sussytoons/__init__.py
@@ -8,11 +8,12 @@ from core.providers.domain.entities import Chapter, Pages, Manga
 class NewSussyToonsProvider(Base):
     name = 'New Sussy Toons'
     lang = 'pt_Br'
-    domain = ['new.sussytoons.site']
+    domain = ['new.sussytoons.site', 'www.sussyscan.com']
 
     def __init__(self) -> None:
         self.base = 'https://api-dev.sussytoons.site'
         self.CDN = 'https://cdn.sussytoons.site'
+        self.old = 'https://oldi.sussytoons.site/wp-content/uploads/WP-manga/data/'
     
     def getManga(self, link: str) -> Manga:
         match = re.search(r'/obra/(\d+)', link)
@@ -33,16 +34,15 @@ class NewSussyToonsProvider(Base):
 
     def getPages(self, ch: Chapter) -> Pages:
         response = Http.get(f'{self.base}/capitulos/{ch.id}').json()
-        list = []
-        for pg in response['resultado']['cap_paginas']:
-            if response['resultado']['cap_imagem'] == None:
-                if "?t=" in pg['src'] and pg['src'].startswith("?t="):
-                    continue
-                page = f'{self.CDN}/storage/wp-content/uploads/WP-manga/data/{pg['src']}?cache=/scans/1/obras/{response['resultado']['obra']['obr_id']}/capitulos/{response['resultado']['cap_numero']}'
-            else:
-                if "?t=" in pg['src'] and pg['src'].startswith("?t="):
-                    continue
-
-                page = f'{self.CDN}/scans/1/obras/{response['resultado']['obra']['obr_id']}/capitulos/{response['resultado']['cap_numero']}/{pg['src']}'
-            list.append(page)
-        return Pages(ch.id, ch.number, ch.name, list)
+        try:
+            response = Http.get(f'{self.base}/{response["obra"]["scan_id"]}/capitulos/{ch.id}', headers={'scan-id': '1'}).json()
+        except Exception as e:
+            print(e)
+        try:
+            list = []
+            for pg in response['resultado']['cap_paginas']:
+                page = f'{self.old}{pg['src']}'
+                list.append(page)
+            return Pages(ch.id, ch.number, ch.name, list)
+        except Exception as e:
+            print(e)

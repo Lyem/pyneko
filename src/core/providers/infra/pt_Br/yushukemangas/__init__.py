a4af14d76b7a68d5f2cc7e65d10d9e69036c578d
MrZero <yanemmanuel00@gmail.com>
2025-02-03 09:21:01 -0300

    fix: yushuken mangas

--- src/core/providers/infra/pt_Br/yushukemangas/__init__.py
+++ src/core/providers/infra/pt_Br/yushukemangas/__init__.py
@@ -4,47 +4,42 @@ from core.__seedwork.infra.http import Http
 from core.providers.infra.template.base import Base
 from core.providers.domain.entities import Chapter, Pages, Manga
 
-class YoshukeMangasProvider(Base):
-    name = 'Yoshuke Mangas'
-    lang = 'pt-Br'
-    domain = ['yushukemangas.com']
+class YushukeMangasProvider(Base):
+    name = 'Yushuke Mangas'
+    lang = 'pt_Br'
+    domain = ['new.yushukemangas.com']
 
     def __init__(self) -> None:
-        self.base = "https://yushukemangas.com/"
+        self.base = 'https://new.yushukemangas.com'
+        self.last_logo = '/uploads/começo_e_fim/fim.png'
     
     def getManga(self, link: str) -> Manga:
         response = Http.get(link)
         soup = BeautifulSoup(response.content, 'html.parser')
-        title = soup.select_one('h1')
-        return Manga(link, title.get_text().strip())
+        title = soup.select_one('div.manga-title-row h1')
 
-    def getChapters(self, link: str) -> List[Chapter]:
-        list = []
-        response = Http.get(link)
+        return Manga(link, title.get_text(strip=True))
+
+    def getChapters(self, id: str) -> List[Chapter]:
+        response = Http.get(id)
         soup = BeautifulSoup(response.content, 'html.parser')
-        title = soup.select_one('h1')
-        while True:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            chaplist = soup.select_one('section.chapter-list')
-            chapters = chaplist.select('a.chapter-link')
-            for chapter in chapters:
-                number = chapter.select_one('span')
-                list.append(Chapter(f"{self.base}{chapter.get('href')}", number.get_text().strip(), title.get_text().strip()))
-            next = soup.select_one('a.pagination-next')
-            if next:
-                response = Http.get(f"{self.base}{next.get("href")}")
-            else:
-                break
-
-        return list
-
-
-    def getPages(self, ch: Chapter) -> Pages:
+        title = soup.select_one('div.manga-title-row h1')
+        get_chapter = soup.select('div#chapters-container a')
         list = []
+        for ch in get_chapter:
+            number = ch.select_one('span.chapter-number')
+            list.append(Chapter(f'{self.base}{ch.get('href')}', number.get_text(strip=True), title.get_text(strip=True)))
+        return list
+    
+    def getPages(self, ch: Chapter) -> Pages:
         response = Http.get(ch.id)
         soup = BeautifulSoup(response.content, 'html.parser')
-        pages_div = soup.select_one('div.chapter-images')
-        images = pages_div.select('img')
-        for img in images:
-            list.append(f"{self.base}{img.get('src')}")
+        get_pages = soup.select('div.manga-container img')
+        list = []
+        for pgs in get_pages:
+            pages = pgs.get('src')
+            if pages == self.last_logo:
+                continue
+            list.append(f'{self.base}{pages}')
         return Pages(ch.id, ch.number, ch.name, list)
+

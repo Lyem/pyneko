091aeea16f1e6af30fdec61dc1572e8c012711db
MrZero <yanemmanuel00@gmail.com>
2025-02-21 23:31:51 -0300

    fix: strellascan

--- src/core/providers/infra/pt_Br/strellascan/__init__.py
+++ src/core/providers/infra/pt_Br/strellascan/__init__.py
@@ -1,4 +1,8 @@
-
+import re
+from typing import List
+from bs4 import BeautifulSoup
+from core.__seedwork.infra.http import Http
+from core.providers.domain.entities import Chapter
 from core.providers.infra.template.blogger_cms import BloggerCms
 
 class StrellaScanProvider(BloggerCms):
@@ -10,6 +14,20 @@ class StrellaScanProvider(BloggerCms):
         self.get_title = 'header h1'
         self.API_domain = 'www.strellascan.xyz'
         self.get_pages = 'div.separator a img'
+    
+    def getChapters(self, id: str) -> List[Chapter]:
+        response = Http.get(id)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.select_one(self.get_title)
+        cleaned_title = re.sub(r"[^\w\sáéíóúâêîôûãõçÁÉÍÓÚÂÊÎÔÛÃÕÇ]", "", title.get_text(strip=True))
+        list = []
+        response = Http.get(f'https://{self.API_domain}/feeds/posts/default/-/{cleaned_title}?alt=json&start-index=1&max-results=150').json()
+        if response['feed']['entry']:
+            for ch in response['feed']['entry']:
+                if ch['link'][4]['href'] == id:
+                    continue
+                list.append(Chapter(ch['link'][4]['href'], ch['title']['$t'], title.get_text(strip=True)))
+        return list
 
 
  

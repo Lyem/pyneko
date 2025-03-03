acc25713eb09ed0ca27368c248ec6d6220bd1059
MrZero <yanemmanuel00@gmail.com>
2025-03-03 19:29:48 -0300

    fix: windscan

--- src/core/providers/infra/pt_Br/windscan/__init__.py
+++ src/core/providers/infra/pt_Br/windscan/__init__.py
@@ -1,9 +1,18 @@
-from core.providers.infra.template.scan_madara_clone import ScanMadaraClone
+from core.providers.infra.template.wordpress_madara import WordPressMadara
 
-class WindScanProvider(ScanMadaraClone):
-    name = 'Wind Scan'
+class WindScansProvider(WordPressMadara):
+    name = 'Wins Scans'
     lang = 'pt_Br'
     domain = ['windscan.xyz']
 
     def __init__(self):
-        self.url = 'https://windscan.xyz/'   
+        self.url = 'https://windscan.xyz/manga/a-vila-quer-voltar-pra-casa/'
+
+        self.path = ''
+        
+        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
+        self.query_chapters = 'li.wp-manga-chapter > a'
+        self.query_chapters_title_bloat = None
+        self.query_pages = 'div.page-break'
+        self.query_title_for_uri = 'head meta[property="og:title"]'
+        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'

6ee8f5887858b32d011ecf15700411b3d2fe049d
MrZero <yanemmanuel00@gmail.com>
2025-03-10 16:29:49 -0300

    feat: enhance HuntersScanProvider with page retrieval functionality

--- src/core/providers/infra/pt_Br/huntersscan/__init__.py
+++ src/core/providers/infra/pt_Br/huntersscan/__init__.py
@@ -1,6 +1,11 @@
+import re
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 from fake_useragent import UserAgent
+from core.__seedwork.infra.http import Http
 from core.providers.domain.entities import Pages
 from core.download.application.use_cases import DownloadUseCase
+from core.providers.domain.entities import Chapter, Pages, Manga
 from core.providers.infra.template.wordpress_madara import WordPressMadara
 
 class HuntersScanProvider(WordPressMadara):
@@ -21,9 +26,26 @@ class HuntersScanProvider(WordPressMadara):
         self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'
         ua = UserAgent()
         user = ua.chrome
-        self.headers = {'host': 'huntersscan.net', 'user_agent': user, 'referer': f'{self.url}/series'}
+        self.headers = {'host': 'hunterscomics.com', 'user_agent': user, 'referer': f'{self.url}/series', 'Cookie': 'acesso_legitimo=1'}
         self.timeout=3
 
+    def getPages(self, ch: Chapter) -> Pages:
+        try:
+            request = Http.get(ch.id)
+            # print(f'[no-render] {request.content}')
+            soup = BeautifulSoup(request.content, 'html.parser')
+            # get_div_images = soup.select(self.query_pages)
+            # print('pegou1')
+            get_images = soup.select(f'{self.query_pages} img')
+            list = []
+            # print('list')
+            for img in get_images:
+                images = img.get('data-src')
+                list.append(images.split('=')[1])
+        except Exception as e:
+            print(e)
+            list = []
+        return Pages(ch.id, ch.number, ch.name, list)
     
     def download(self, pages: Pages, fn: any, headers=None, cookies=None):
         if headers is not None:

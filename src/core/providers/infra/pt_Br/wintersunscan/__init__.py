305b1e75c7d8e380b777b048b05e8e0cda2d9cf2
MrZero <yanemmanuel00@gmail.com>
2025-07-05 01:31:27 -0300

    feat: implement WinterSunScanProvider with manga and chapter extraction methods

--- src/core/providers/infra/pt_Br/wintersunscan/__init__.py
+++ src/core/providers/infra/pt_Br/wintersunscan/__init__.py
@@ -1,38 +1,42 @@
-from core.providers.infra.template.wordpress_madara import WordPressMadara
+from typing import List
+from bs4 import BeautifulSoup
 from core.__seedwork.infra.http import Http
-from urllib.parse import urljoin
-from core.download.application.use_cases import DownloadUseCase
-from core.providers.domain.entities import Pages
+from core.providers.infra.template.base import Base
+from core.providers.domain.entities import Chapter, Pages, Manga
 
-class WinterSunScanProvider(WordPressMadara):
-    name = 'Winter sun scan'
-    lang = 'pt-Br'
-    domain = ['wintersunscan.xyz']
+class WinterSunScanProvider(Base):
+    name = 'Winter Sun Scan'
+    lang = 'pt_Br'
+    domain = ['wintersunscan.com']
 
-    def __init__(self):
-        self.url = 'https://wintersunscan.xyz'
-
-        self.path = ''
-        
-        self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
-        self.query_chapters = 'li.wp-manga-chapter > a'
-        self.query_chapters_title_bloat = None
-        self.query_pages = 'div.page-break.no-gaps'
-        self.query_title_for_uri = 'head meta[property="og:title"]'
-        self.query_placeholder = '[id^="manga-chapters-holder"][data-id]'
+    def __init__(self) -> None:
+        pass
     
-    def download(self, pages: Pages, fn: any, headers=None, cookies=None):
-        if headers is not None:
-            headers = headers | {'Referer': 'https://wintersunscan.xyz/lixo'}
-        else:
-            headers = {'Referer': 'https://wintersunscan.xyz/lixo'}
-        return DownloadUseCase().execute(pages=pages, fn=fn, headers=headers, cookies=cookies)
-    
-    def _get_chapters_ajax(self, manga_id):
-        uri = urljoin(self.url, f'{manga_id}ajax/chapters/')
-        response = Http.post(uri, headers={'Cookie': 'visited=true; wpmanga-reading-history=W3siaWQiOjg4MiwiYyI6IjIzMzU5IiwicCI6MSwiaSI6IiIsInQiOjE3MTk5NjEwODN9XQ%3D%3D'})
-        data = self._fetch_dom(response, self.query_chapters)
-        if data:
-            return data
-        else:
-            raise Exception('No chapters found (new ajax endpoint)!')
+    def getManga(self, link: str) -> Manga:
+        response = Http.get(link)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.select_one('div.name-rating.mb-24 h2')
+        return Manga(link, title.get_text(strip=True))
+
+
+    def getChapters(self, id: str) -> List[Chapter]:
+        response = Http.get(id)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.select_one('div.name-rating.mb-24 h2')
+        get_chapters_div = soup.select_one('div.specmobile.row')
+        list = []
+        for ch in get_chapters_div:
+            onclick = ch.select_one('div.card-caps.d-flex.align-items-center')
+            url = onclick.get('onclick').split("'")[1]
+            list.append(Chapter(url, ch.select_one('span.color-white.d-block').get_text(strip=True), title.get_text(strip=True)))
+        return list
+
+    def getPages(self, ch: Chapter) -> Pages:
+        response = Http.get(ch.id)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        images = soup.select('div.d-flex.justify-content-center.align-items-center.flex-column img')
+        list = []
+        for img in images:
+            list.append(img.get('src'))
+        return Pages(ch.id, ch.number, ch.name, list)
+

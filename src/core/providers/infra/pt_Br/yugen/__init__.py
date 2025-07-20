fa75ec19df8d65b5fcbd3361762766c15c7516db
MrZero <118790546+YanEmmanuel@users.noreply.github.com>
2025-07-20 19:36:17 -0300

    Atualizar o __init__.py

--- src/core/providers/infra/pt_Br/yugen/__init__.py
+++ src/core/providers/infra/pt_Br/yugen/__init__.py
@@ -10,7 +10,7 @@ class YugenProvider(Base):
     domain = [re.compile(r'\byugenbr\.[^\s/]+(?:\.[^\s/]+)*\b')]
 
     def __init__(self) -> None:
-        self.base = 'https://yugenbr.nssec.xyz/'
+        self.base = 'https://yugenmangasbr.yocat.xyz/'
         self.cdn = 'https://media.yugenweb.com/'
         self.api = 'https://api.yugenweb.com/'
         self.headers = {'referer': f'{self.base}'}
